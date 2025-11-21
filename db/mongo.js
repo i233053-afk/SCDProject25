b0ab97cdfa5e521d15e3c03fc6aae4ffbe0232b0
Manahil Fatima <i233053@isb.nu.edu.pk>
2025-11-21 13:47:53 +0500

    .env added

--- db/mongo.js
+++ db/mongo.js
@@ -1,12 +1,15 @@
+// db/mongo.js
 const mongoose = require("mongoose");
+require("dotenv").config(); // Load .env variables
 
 async function connectMongo() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/nodevault");
-
+    await mongoose.connect(process.env.MONGO_URI, {
+      // Node.js Driver 4+ doesn't need useNewUrlParser or useUnifiedTopology
+    });
     console.log("✅ Connected to MongoDB successfully!");
   } catch (err) {
-    console.error("❌ MongoDB Connection Failed:", err.message);
+    console.error("❌ MongoDB Connection Failed:", err);
   }
 }
 
